4c0d27b461a732f9f4abf365b0f5840e62da8c6e
Adhamebrahim <92182467+adhamebraheem@users.noreply.github.com>
2023-09-14 19:37:11 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+document.getElementById("mfa_root");
+
+
+function u() {
+    if (n = _ge("mfa_root"),
+    n)
+        if (fab_config && fab_config.micFabAlwaysVisible) {
+            var u = _ge("mfa_vsrch");
+            u ? (sj_evt.bind("vs_fab_vis_feature", i, !0),
+            r(!0, t)) : sj_evt.bind("fab_vis", i, !0)
+        } else
+            sj_evt.bind("fab_vis", i, !0)
+}
